42c925ff52e83ddbd79fe229946d341569c67e8b
Vimal Patel <94vimalpatel@gmail.com>
2020-03-20 23:12:20 +0530

    Add client: landing, login, and register

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { Register } from "./components/auth/Register";
+import { Login } from "./components/auth/Login";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Navbar></Navbar>
+      <Route exact path="/" component={Landing}></Route>
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </Switch>
+      </section>
+    </Router>
+  </Provider>
+);
+
+export default App;
