68cacf9f5ad3aee2a9c21d55ae9646b96adcb630
Vimal Patel <94vimalpatel@gmail.com>
2020-03-27 20:53:26 +0530

    Write login, logout, and dashboard components

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -23,23 +25,30 @@ if (localStorage.token) {
 const App = () => {
   // useEffect: Functional component equivalent of componentDidMount in a class component
   // for it to run just once like componentDidMount, pass [] as the second argument
-  useEffect (() => {
+  useEffect(() => {
     store.dispatch(loadUser());
   }, []);
-  return(
-  <Provider store={store}>
-    <Router>
-      <Navbar></Navbar>
-      <Route exact path="/" component={Landing}></Route>
-      <section className="container">
-        <Alert></Alert>
-        <Switch>
-          <Route exact path="/register" component={Register}></Route>
-          <Route exact path="/login" component={Login}></Route>
-        </Switch>
-      </section>
-    </Router>
-  </Provider>
-)};
+  return (
+    <Provider store={store}>
+      <Router>
+        <Navbar></Navbar>
+        <Route exact path="/" component={Landing}></Route>
+        <section className="container">
+          <Alert></Alert>
+          <Switch>
+            <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/login" component={Login}></Route>
+            {/* Our customized react-router-dom Route */}
+            <PrivateRoute
+              exact
+              path="/dashboard"
+              component={Dashboard}
+            ></PrivateRoute>
+          </Switch>
+        </section>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,7 +4,10 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT
 } from "./types";
 import setAuthToken from "../utils/setAuthToken";
 
@@ -36,6 +39,7 @@ export const register = ({ name, email, password }) => async dispatch => {
   try {
     const res = await axios.post("api/users", { name, email, password });
     dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+    dispatch(loadUser());
   } catch (e) {
     const errors = e.response.data.errors;
     if (errors) {
@@ -45,4 +49,26 @@ export const register = ({ name, email, password }) => async dispatch => {
       type: REGISTER_FAIL
     });
   }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+  try {
+    const res = await axios.post("api/auth", { email, password });
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+    dispatch(loadUser());
+  } catch (e) {
+    const errors = e.response.data.errors;
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+    dispatch({
+      type: LOGIN_FAIL
+    });
+  }
+};
+
+// Logout / Clear Profile
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
