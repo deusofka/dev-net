89c7c863d97a29d3306849ff7a2a89134ff544c2
Vimal Patel <94vimalpatel@gmail.com>
2020-04-08 08:02:33 +0530

    Prepare for deployment

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,7 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Alert from "./components/layout/Alert";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
-import PrivateRoute from "./components/routing/PrivateRoute";
+import Routes from "./components/routing/Routes";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -40,49 +28,11 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Navbar></Navbar>
-        <Route exact path="/" component={Landing}></Route>
-        <section className="container">
-          <Alert></Alert>
-          <Switch>
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/profiles" component={Profiles} />
-            <Route exact path="/profiles/:id" component={Profile} />
-            {/* Our customized react-router-dom Route */}
-            <PrivateRoute
-              exact
-              path="/dashboard"
-              component={Dashboard}
-            ></PrivateRoute>
-            <PrivateRoute
-              exact
-              path="/create-profile"
-              component={CreateProfile}
-            ></PrivateRoute>
-            <PrivateRoute
-              exact
-              path="/edit-profile"
-              component={EditProfile}
-            ></PrivateRoute>
-            <PrivateRoute
-              exact
-              path="/add-experience"
-              component={AddExperience}
-            ></PrivateRoute>
-            <PrivateRoute
-              exact
-              path="/add-education"
-              component={AddEducation}
-            ></PrivateRoute>
-            <PrivateRoute exact path="/posts" component={Posts}></PrivateRoute>
-            <PrivateRoute
-              exact
-              path="/posts/:id"
-              component={Post}
-            ></PrivateRoute>
-          </Switch>
-        </section>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Landing}></Route>
+          <Route component={Routes}></Route>
+        </Switch>
       </Router>
     </Provider>
   );
--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 // Call method to config db
 require("./config/db.js")();
-// Look for an env var called PORT to use when deployed on Heroku
-// Locally, run on port 5000
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server stated on port ${PORT}`));
+
 // body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  res.send("API running");
-});
 app.use("/api/users/", require("./routes/api/users"));
 app.use("/api/auth/", require("./routes/api/auth"));
 app.use("/api/profiles/", require("./routes/api/profiles"));
 app.use("/api/profile/", require("./routes/api/profile"));
 app.use("/api/github/", require("./routes/api/github"));
 app.use("/api/posts/", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  // Serve index.html file
+  // * -- any other route other than the above
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// Look for an env var called PORT to use when deployed on Heroku
+// Locally, run on port 5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server stated on port ${PORT}`));
